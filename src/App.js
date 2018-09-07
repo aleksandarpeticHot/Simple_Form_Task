546513d74329b9bc61eb4145afb8358a84db3b4b
aleksandar_petic <aleksandar.petic@bitlab.rs>
2018-09-07 21:18:31 +0200

    I II III dio Task forme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
  import React, { Component } from 'react';
 import './App.css';
-import { Dropdown,Button,Modal } from 'semantic-ui-react';
+import {required} from  './validation';
+import { Dropdown,Button,Modal,Form,Checkbox, FormField } from 'semantic-ui-react';
 const inlineStyle = {
   modal : {
     marginTop: '50px '
@@ -22,50 +23,74 @@ const optionsStatus= [
   { key: 3, text: 'Done', value: 'done' },
 ]
 const optionsPort= [
-  { key: 1, text: '1', value: '2' },
+  { key: 1, text: '1', value: '1' },
   { key: 2, text: '2', value: '2' }
 ]
 
 class App extends Component {
-    state = { open: false }
+  constructor(){
+    super();
+    this.state = { open: false ,
+      value:"",
+      dValue:"",
+    lista:[]}
+    this.handleChangePin=this.handleChangePin.bind(this);
+    this.handleChangeStatus=this.handleChangeStatus.bind(this);
+    this.handleSubmit=this.handleSubmit.bind(this);
+  }
 
-  show = size => () => this.setState({ size, open: true });
+  show = size => () => this.setState({ size, open: true,name:size });
   close = () => this.setState({ open: false });
-    
+  handleChangeStatus(e,value){
+    this.setState({value:value.value});
+
+  }  
+  handleChangePin(e,value){
+    this.setState({dValue:value.value});
+  }
   
+  handleSubmit(event){
+    console.log("Status ",this.state.value);
+    console.log("Port ",this.state.dValue);    
+    this.setState({open:false});
+    
+  }
 
   render() {
     const { open, size } = this.state
     return (
       <div className='app'>
-      <Button content="Add Status"onClick={this.show('tiny')} style={inlineStyle.modal}  />
-      <Button content="Edit Status" onClick={this.show('tiny')}/>
-    
-      <Modal size={size} open={open} onClose={this.close} style={modalStyle.modal}>
-          <Modal.Header>Add new status</Modal.Header>
-          <Modal.Content>
-          <Dropdown
-             search
-             selection
-             options={optionsStatus}
-             placeholder='Status'
-            
-           />
-           <br></br>
-           <Dropdown
-             search
-             selection
-             options={optionsPort}
-             placeholder='Attach a Port'
-            
-           />
-           
+      <Button content="Add Status"onClick={this.show('Add new Status')} style={inlineStyle.modal}   />
+      <Button content="Edit Status" onClick={this.show('Edit Status')}/>    
+      <Modal size={'tiny'} open={open} onClose={this.close} style={modalStyle.modal}>
+          <Modal.Header>{this.state.name}</Modal.Header>
+         
+          <Modal.Content>   
+              <Form id="forma1" onSubmit={this.handleSubmit} > 
+               <Dropdown
+               validate={required}                
+                search
+                selection
+                options={optionsStatus}
+                placeholder='Status'  
+                onChange={this.handleChangeStatus}/>
+              <br></br> 
+              <Dropdown
+                search
+                selection
+                options={optionsPort}
+                placeholder='Attach a Port'
+                onChange={this.handleChangePin}/> 
+               </Form>           
           </Modal.Content>
-          <Modal.Actions>
-            <Button negative onClick={this.close}>Cancel</Button>
-            <Button positive icon='checkmark' labelPosition='right' content='Save' />
+          <Modal.Actions>    
+             <Button negative onClick={this.close}>Cancel</Button>
+            <Button form="forma1" type="submit" positive icon='checkmark' labelPosition='right' content='Save' />  
+                    
           </Modal.Actions>
+         
         </Modal>
+       
       </div>
     );
   }
